51f50dc6041b0dc729b00a836b5c69fc2de0d02b
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-02-11 14:16:09 +0900

    Create main.java

---
+++ src/main/java/com/ddnsgeek/approximasterstudios2004/modloader/main.java
@@ -0,0 +1,47 @@
+package com.ddnsgeek.approximasterstudios2004.modloader;
+
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import net.internationalx.api.TranslationManager;
+
+
+public class main extends JavaPlugin implements SlimefunAddon {
+
+  String ver = "1.20.6-R000-SNAPSHOT";
+  String MC = "1.20.6";
+  String Cheat = "1.20.6-R0.1-SNAPSHOT"
+  
+  @Override
+  public void onLoad() {
+    System.out.println("Loading Approximaster " + ver + "for " + mc + "...");
+    TranslationManager translationManager = new TranslationManager(config);
+    System.out.println(TranslationManager.translate("mm_welcome", "User"));
+  }
+  
+	@Override
+	public void onEnable() {
+
+  }
+	
+	@Override
+	public void onDisable() {
+		// Logic for disabling the plugin...
+	}
+	
+	@Override
+	public JavaPlugin getJavaPlugin() {
+		// This is a method that links your SlimefunAddon to your Plugin.
+		// Just return "this" in this case, so they are linked
+		return this;
+	}
+	
+	@Override
+	public String getBugTrackerURL() {
+		// Here you can return a link to your Bug Tracker.
+		// This link will be displayed to Server Owners if there is an issue
+		// with this Addon. Return null if you have no bug tracker.
+		// Normally you can just use GitHub's Issues tab:
+		// https://github.com/YOURNAME/YOURPROJECT/issues
+		return "https://github.com/GAME-CLI-SRV-DEV/ApproximasterModLoader/issues";
+	}
+
+}
